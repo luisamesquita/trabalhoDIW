2de143b5fb13a58f5bd52d2f6e3885ee99dc2e13
luisamesquita <luisa.mesquita20@gmail.com>
2022-11-15 16:36:56 -0300

    Arquivos sendo adicionados

---
+++ script.js
@@ -0,0 +1,11 @@
+function carregaInfos () {
+    let http = new XMLHttpRequest();
+    http.onload = function () {
+        
+    }
+}
+
+function carregaMaisDetalhes () {
+    let infos = '';
+    document.querySelector('#maisDetalhes') = infos
+}
